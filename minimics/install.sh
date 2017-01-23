9439f81e255a2f9950e4c74c9f20060366bff0ce
rguichard <rguichard@vsense.fr>
2017-01-23 11:25:27 +0100

    update

---
+++ minimics/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "your username :"
+read username
+echo "git username :"
+read git_username
+echo "git email address :"
+read git_email_addr
+echo "Ubuntu code name version (trusty, karmic, xenial, yakkety etc) :"
+read ubuntu_version
+echo "Is your machine a desktop or server one ? (desktop/server) :"
+read machine_type
+
+echo "username: $username" >> group_vars/all
+echo "machine_type: $machine_type" >> group_vars/all
